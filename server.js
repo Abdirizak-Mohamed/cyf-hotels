3412a9a7fec370f30a9ab36cf7e1596ae33c1839
Abdirizak Mohamed <abdi.mohamed@zoopla.co.uk>
2022-06-25 14:37:59 +0100

    feat - delete p.jon

--- server.js
+++ server.js
@@ -17,7 +17,30 @@ app.get("/hotels", function(req, res) {
             res.status(500).json(error);
         });
 });
-const port = process.env.PORT
-app.listen(port, function() {
-    console.log(`Server is listening on port ${port}. Ready to accept requests!`);
+
+app.get("/bookings", function(req, res) {
+    pool
+        .query("SELECT * FROM bookings")
+        .then((result) => res.json(result.rows))
+        .catch((error) => {
+            console.error(error);
+            res.status(500).json(error);
+        });
 });
+
+app.get("/customers", function(req, res) {
+    pool
+        .query("SELECT * FROM customers")
+        .then((result) => res.json(result.rows))
+        .catch((error) => {
+            console.error(error);
+            res.status(500).json(error);
+        });
+});
+
+const port = process.env.PORT
+app.listen(4000, function() {
+    console.log(`Server is listening on port ${3000}. Ready to accept requests!`);
+});
+
+//export DATABASE_URL=postgres://ygwmshlxhlxldp:29336236b2d2a3f566162bdb57811f03d64a135c074c3b77fd174c7af27bfd19@ec2-34-248-169-69.eu-west-1.compute.amazonaws.com:5432/d6n4mps1bqfotu
